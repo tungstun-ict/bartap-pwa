4ec0e49d048bb62cd624a4b5364d557af4e5d5c8
JortWillemsen <jort@socialbrothers.nl>
2022-05-30 12:53:23 +0200

    Add menu selected option

--- src/stories/menu-link/tungstun-menu-link.js
+++ src/stories/menu-link/tungstun-menu-link.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-import "./tungstun-menu-link.scss"
+import "./tungstun-menu-link.scss";
 
-function TungstunMenuLink({to, text}) {
+const TungstunMenuLink = ({ to, text }) => {
+  const location = useLocation();
+
+  const selected = location.pathname === to;
   return (
-    <div className="menu-link__container">
-      <Link className="menu-link" to={to}>{text}</Link>
+    <div className={`menu-link__container ${selected && "menu-link-active"}`}>
+      <Link className="menu-link" to={to}>
+        {text}
+      </Link>
     </div>
   );
-}
+};
 
 export default TungstunMenuLink;
