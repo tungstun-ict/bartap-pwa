a7d97382d9a1b0ce89b1c9708cd5ffce93117d81
JortWillemsen <jort@maurice-nienke.nl>
2022-06-15 22:52:46 +0200

    Add QR reading

--- src/pages/scan-page/ScanPage.js
+++ src/pages/scan-page/ScanPage.js
@@ -1,28 +1,71 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { QrReader } from "react-qr-reader";
+import { useLocation, useNavigate } from "react-router-dom";
+import TungstunNotificationContext from "../../stories/notification/tungstun-notification-provider";
+import TungstunInput from "../../stories/input/tungstun-input";
 import TungstunPage from "../../stories/page/tungstun-page";
 import TungstunTitle from "../../stories/title/tungstun-title";
 
 import "./scan-page.scss";
+import TungstunForm from "../../stories/form/tungstun-form";
+import TungstunTextButton from "../../stories/text-button/tungstun-text-button";
+import useForm from "../../utils/useForm";
+import TungstunIconButton from "../../stories/icon-button/tungstun-icon-button";
 
 const ScanPage = () => {
-  const [data, setData] = useState("No result");
+  const dispatch = useContext(TungstunNotificationContext);
+  const navigate = useNavigate();
+  const [scan, setScan] = useState(null);
+  const [formValues, updateFormValues] = useForm();
+
+  useEffect(() => {
+    if (scan !== null) {
+      handleScan(scan);
+    }
+  }, [scan]);
+
+  const handleScan = (input) => {
+    dispatch({
+      type: "ADD_NOTIFICATION",
+      payload: { text: "Succesfully added new bar" },
+    });
+    navigate(`/bar/${input}`);
+  };
 
   return (
     <TungstunPage>
       <TungstunTitle text="📷 Scan QR" level={1} back />
-      <QrReader
-        style={{ width: "250px", height: "20px" }}
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-          }
-
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-      />
+      <div className="scan-page__camera">
+        {scan === null ? (
+          <QrReader
+            constraints={{
+              facingMode: "environment",
+            }}
+            scanDelay={500}
+            onResult={(result, error) => {
+              if (!!result && scan === null) {
+                setScan(result);
+              }
+            }}
+          />
+        ) : (
+          "Oops"
+        )}
+      </div>
+      <div className="scan-page__form">
+        <TungstunForm onSubmit={() => handleScan(formValues.id)}>
+          <TungstunInput
+            hint={"Id"}
+            name="id"
+            value={formValues.id}
+            onChange={updateFormValues}
+          />
+          <TungstunIconButton
+            src={require("../../assets/icons/arrow-light.png")}
+            onClick={() => handleScan(formValues.id)}
+          />
+        </TungstunForm>
+      </div>
     </TungstunPage>
   );
 };
--- src/stories/notification/tungstun-notification-queue.js
+++ src/stories/notification/tungstun-notification-queue.js
@@ -4,11 +4,24 @@ import "./tungstun-notification-queue.scss";
 import TungstunNotificationContext from "./tungstun-notification-provider";
 import TungstunNotification from "./tungstun-notification";
 
-const TungstunNotificationQueue = ({state, dispatch}) => {
+const TungstunNotificationQueue = ({ state, dispatch }) => {
+  console.log(state);
   return (
-    <div className="notification-queue__container">
-      {state.map((n) => (<TungstunNotification key={n.id} id={n.id} text={n.text} error={n.error} dispatch={dispatch} />))}
-    </div>
+    <>
+      {state.length > 0 && (
+        <div className="notification-queue__container">
+          {state.map((n) => (
+            <TungstunNotification
+              key={n.id}
+              id={n.id}
+              text={n.text}
+              error={n.error}
+              dispatch={dispatch}
+            />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
