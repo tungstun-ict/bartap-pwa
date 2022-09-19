24b93495cf114269f04257239278684bb46a1ab8
JortWillemsen <jort@maurice-nienke.nl>
2022-09-19 17:48:18 +0200

    Add list searching

--- src/stories/list-view/tungstun-list-view.js
+++ src/stories/list-view/tungstun-list-view.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import TungstunListItem from "../list-item/tungstun-list-item";
 import TungstunSearchBar from "../search-bar/tungstun-search-bar.tsx";
 import "./tungstun-list-view.scss";
@@ -8,18 +8,32 @@ import "./tungstun-list-view.scss";
 function TungstunListView({ children }) {
   const [search, setSearch] = useState("");
   const [filteredChildren, setFilteredChildren] = useState(children);
-  
+
+  const searchProps = (props) => {
+    let toFilter = Object.values(props).filter((value) => {
+      return typeof value === "object";
+    });
+
+    for (let item of toFilter) {
+      for (let value of Object.values(Object.values(item))) {
+        console.log(value);
+
+        if (typeof value !== "string") {
+          continue;
+        }
+
+        if (value.toLowerCase().includes(search.toLowerCase())) {
+          return true;
+        }
+      }
+    }
+  };
+
   useEffect(() => {
     if (search === "") {
       setFilteredChildren(children);
     } else {
-      setFilteredChildren(
-        children.filter((c) => c.props.forEach((prop) => {
-          if (prop.value.toLowerCase().includes(search.toLowerCase())) {
-            return true;
-          }
-        }))
-      );
+      setFilteredChildren(children.filter((c) => searchProps(c.props)));
     }
   }, [search, children]);
 
@@ -30,7 +44,7 @@ function TungstunListView({ children }) {
         onChange={(e) => {
           setSearch(e.target.value);
         }}
-        />
+      />
       {filteredChildren}
     </div>
   );
